dc924ef9fe09e74d41e9cb333674a23b6ed25116
iv172 <iv172@hotmail.com>
2023-11-28 14:29:32 +0900

    init

---
+++ src/assets/Medium.tsx
@@ -0,0 +1,24 @@
+export default function Medium() {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="medium"
+    >
+      <rect width="48" height="48" rx="24" fill="#212121" />
+      <ellipse cx="18.8091" cy="24" rx="6.62702" ry="6.5" fill="white" />
+      <ellipse cx="29.8528" cy="24" rx="3.31351" ry="6.5" fill="white" />
+      <rect
+        x="34.2717"
+        y="17.5"
+        width="1.5463"
+        height="13"
+        rx="0.773152"
+        fill="white"
+      />
+    </svg>
+  )
+}
