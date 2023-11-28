9ed46afc0878a40c9cb908e8dba68dfc6fc648b0
iv172 <iv172@hotmail.com>
2023-11-28 20:38:52 +0900

    change favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -179,7 +179,7 @@ export default function Home() {
         <title>Bounce Bitcoin</title>
         <meta name="description" content="Bounce Bitcoin" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <div className={`scroll ${styles.scroll}`}></div>
       <div className={`${styles.scroll2}`}></div>
